99d193db46118b2f418808141124fa638d926c5b
Ansh7272 <120785449+Ansh7272@users.noreply.github.com>
2025-12-04 01:31:37 +0530

    "fix image load "

--- next.config.mjs
+++ next.config.mjs
@@ -1,37 +1,17 @@
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [{
-            protocol: "https",
-            hostname: "randomuser.me"
-        },],
-    },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "randomuser.me",
+        port: "",
+        pathname: "/api/portraits/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
-
-
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   images: {
-//     domains: ["randomuser.me"],
-//   },
-// };
-
-// export default nextConfig;
-
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   images: {
-//     remotePatterns: [
-//       {
-//         protocol: "https",
-//         hostname: "randomuser.me",
-//         pathname: "/api/portraits/**",
-//       },
-//     ],
-//   },
-// };
-
-// export default nextConfig;
-
