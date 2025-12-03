d06f4965f137a50e0ee20bb12f489eca5b5cdceb
Ansh7272 <120785449+Ansh7272@users.noreply.github.com>
2025-12-04 01:05:43 +0530

    Fix Next.js image config and update remotePatterns

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,37 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     images: {
+//         remotePatterns: [{
+//             protocol: "https",
+//             hostname: "randomuser.me"
+//         },],
+//     },
+// };
+
+// export default nextConfig;
+
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     domains: ["randomuser.me"],
+//   },
+// };
+
+// export default nextConfig;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [{
-            protocol: "https",
-            hostname: "randomuser.me"
-        },],
-    },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "randomuser.me",
+        pathname: "/api/portraits/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
+
